a671c9c9c29dbc88ae354e4f153588c1a66d19be
Valoraken <49834155+Valoraken@users.noreply.github.com>
2019-04-22 23:11:59 +0200

    Add files via upload

--- Dynamic System Estimation/Lab1/Lab1_2.m
+++ Dynamic System Estimation/Lab1/Lab1_2.m
@@ -1,4 +1,6 @@
-
+% Xiao,Tianqi
+% Lab 1 Task 2
+% 21.04.2019
 % the ODE:  y'=-2ty/(y^2-t^2)
 % interval of t: [0,4]
 % stepwidth: h=1
@@ -9,16 +11,19 @@ clc
 % the rewritten function handle
 f=@(t,y) -2*t*y/(y^2-t^2);
 t0=0;
-h=1;
-tmax=5;
+h=0.01;
+tmax=4;
 y0=4;
 % visualize
 [X,Y]= RungeKutta3(f,t0,h,tmax,y0);
 plot(X,Y,'r')
-text(X,Y,'('num2str(X))
-
+fprintf('y(4) by 3rd order method: %f \r',Y(tmax/h+1))
 hold on
 [x,y]=RungeKutta4(f,t0,h,tmax,y0);
 plot(x,y,'b')
-legend('3','4')
-title({'Solution of yÂ¡Â¯=-2ty/(y^2-t^2) ';('t0=0  y0=4')})
+grid on
+fprintf('y(4) by 4th order method: %f \r',y(tmax/h+1))
+legend('3rd order','4th order')
+xlabel('t');
+ylabel('y');
+title({'Solution of y¡¯=-2ty/(y^2-t^2) ';('t0=0  y0=4')})
